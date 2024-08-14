7f01fc5baff6d1293bfe7230619bd69ee2e9da41
Diego Martinez <kirainakami041@gmail.com>
2024-08-13 19:58:00 -0600

    Creación del index.php con estructura html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title> DIEGO PAPSOTE MARTINEZ GARCIA </title>
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/style.css" />
+<script src="js/jquery-3.7.1.min.js" ></script>
+<script src="js/bootstrap.bundle.min.js" ></script>
+</head>
+
+<body>
+<h1> Hola nena </h1>
+</body>
+
+</html>
